dc8b68a910169330d3afc91d912809a08896223d
TuanThanh <maitthanh41020@gmail.com>
2023-03-31 17:46:36 +0700

    add project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: '/Amazon-website/',
     plugins: [
         laravel({
             input: 'resources/js/app.js',
